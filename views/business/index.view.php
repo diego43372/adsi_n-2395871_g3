fb1ab7d5916f49d6d11a60bef5aecdc642a11d34
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-08-08 09:39:55 -0500

    Colores

--- views/business/index.view.php
+++ views/business/index.view.php
@@ -4,8 +4,39 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Empresa</title>
+	<link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
-	<h1>Estoy en la vista de la Empresa (Landing)</h1>
+	<div class="contenedor">
+		<!-- Navegador -->
+		<nav>
+			<!-- Logo -->
+			<div class="logo"></div>
+			<!-- menu -->
+			<div class="menu"></div>
+			<!-- ingreso -->
+			<div class="ingreso"></div>
+		</nav>
+		<!-- Encabezado -->
+		<header class="borde"></header>
+		<!-- Título Portafolio -->
+		<div class="borde titulo"></div>
+		<!-- Sección Portafolio -->
+		<section class="portafolio">
+			<div class="borde port-col"></div>
+			<div class="borde port-col"></div>
+			<div class="borde port-col"></div>
+		</section>
+		<!-- Título Nosotros -->
+		<div class="borde titulo"></div>
+		<!-- Sección Nosotros -->
+		<section class="borde nosotros"></section>
+		<!-- Título Contáctenos -->
+		<div class="borde titulo"></div>
+		<!-- Sección Contáctenos -->
+		<section class="borde contactenos"></section>
+		<!-- Pie de Página -->
+		<footer class="borde"></footer>		
+	</div>
 </body>
 </html>
