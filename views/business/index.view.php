03c1c2bf2abd9ecbc70ef2647ecaa0d728fdbb44
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-08-10 10:24:13 -0500

    Responsive Design Modelo Cajas y Flexbox

--- views/business/index.view.php
+++ views/business/index.view.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Empresa</title>
-	<link rel="stylesheet" href="assets/css/styles-mod-cajas.css">
+	<link rel="stylesheet" href="assets/css/styles-mod-flexbox.css">
 </head>
 <body>
 	<div class="contenedor borde">
@@ -18,38 +18,35 @@
 			<div class="ingreso color-gris-oscuro"></div>
 		</nav>
 		<!-- Encabezado -->
-		<header class="color-blanco"></header>
-		<!-- Título Portafolio -->
-		<div class="titulo color-negro"></div>
+		<header class="color-blanco"></header>		
 		<!-- Sección Portafolio -->
+		<div class="titulo color-negro"></div>
 		<section class="portafolio">
-			<div class="port-col color-gris-oscuro"></div>
-			<div class="port-col color-blanco"></div>
-			<div class="port-col color-gris-claro"></div>
-		</section>
-		<!-- Título Nosotros -->
-		<div class="titulo color-negro"></div>
+			<div class="port-col-izq color-gris-oscuro"></div>
+			<div class="port-col-cnt color-blanco"></div>
+			<div class="port-col-der color-gris-claro"></div>
+		</section>		
 		<!-- Sección Nosotros -->
-		<section class="nosotros">
-			<div class="nos-fila">
-				<div class="nos-col-ext color-gris-claro"></div>
-				<div class="nos-col-cnt color-gris-oscuro"></div>
-				<div class="nos-col-ext color-gris-claro"></div>
-			</div>
-			<div class="nos-fila">
-				<div class="nos-col-ext color-gris-oscuro"></div>
-				<div class="nos-col-cnt color-blanco"></div>
-				<div class="nos-col-ext color-gris-oscuro"></div>
-			</div>
-			<div class="nos-fila">
-				<div class="nos-col-ext color-gris-claro"></div>
-				<div class="nos-col-cnt color-gris-oscuro"></div>
-				<div class="nos-col-ext color-gris-claro"></div>
-			</div>
-		</section>
-		<!-- Título Contáctenos -->
 		<div class="titulo color-negro"></div>
+		<section class="nosotros">
+			<div class="nos-fila-arr">
+				<div class="nos-col-izq color-gris-claro"></div>
+				<div class="nos-col-cnt color-gris-oscuro"></div>
+				<div class="nos-col-der color-gris-claro"></div>
+			</div>
+			<div class="nos-fila-cnt">
+				<div class="nos-col-izq color-gris-oscuro"></div>
+				<div class="nos-col-cnt color-blanco"></div>
+				<div class="nos-col-der color-gris-oscuro"></div>
+			</div>
+			<div class="nos-fila-abj">
+				<div class="nos-col-izq color-gris-claro"></div>
+				<div class="nos-col-cnt color-gris-oscuro"></div>
+				<div class="nos-col-der color-gris-claro"></div>
+			</div>
+		</section>		
 		<!-- Sección Contáctenos -->
+		<div class="titulo color-negro"></div>
 		<section class="contactenos">
 			<div class="cont-col color-gris-oscuro"></div>
 			<div class="cont-col">
