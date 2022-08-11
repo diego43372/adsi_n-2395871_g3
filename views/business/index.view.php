38d32331af27b77191d3282412afd6eea94bae3f
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-08-11 08:38:31 -0500

    Responsive Design Modelo Cajas y Flexbox

--- views/business/index.view.php
+++ views/business/index.view.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Empresa</title>
-	<link rel="stylesheet" href="assets/css/styles-mod-flexbox.css">
+	<link rel="stylesheet" href="assets/css/styles-mod-cajas.css">
 </head>
 <body>
 	<div class="contenedor borde">
