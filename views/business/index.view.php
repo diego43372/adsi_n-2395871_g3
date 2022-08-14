faea6d6e9794212bee63a7983a7964a93668baea
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-08-14 17:09:41 -0500

    Proyecto. Vistas: Logo

--- views/business/index.view.php
+++ views/business/index.view.php
@@ -26,7 +26,9 @@
 		<section class="portafolio">
 			<div class="port-col-izq color-gris-oscuro">
 				<div class="borde subtitulo"></div>
-				<div class="borde imagen"></div>
+				<div class="borde imagen">
+					<img src="assets/img/imagen-card.jpg" alt="Imagen Card" title="Imagen Card">
+				</div>
 				<div class="borde parrafo"></div>
 				<div class="borde ver"></div>
 			</div>
