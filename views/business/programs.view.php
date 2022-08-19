31b6946aec6cec2a37a26ce321500037f379c707
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-08-19 09:54:01 -0500

    Proyecto. Vistas: Implementando más vistas Landing

--- views/business/programs.view.php
+++ views/business/programs.view.php
@@ -1,1 +1,7 @@
-Descripción de los Programas
+		<div class="tope"></div>
+		<div class="titulo"><h1>Programas</h1></div>
+		<section class="paginas">
+			<div class="parrafo">
+				<p>Descripción de los Programas de Formación</p>
+			</div>
+		</section>
