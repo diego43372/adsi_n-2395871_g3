1e348ef4a1ad2cffa4a9242aec6af851135019dc
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-08-23 08:42:19 -0500

    Proyecto. Vistas: Tablas

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$controller->index();			
 	} else {
 		$controller = $_REQUEST['c'];
-		require_once 'controllers/' . $controller. '.php';
+		require_once 'controllers/' . $controller . '.php';
 		$controller = new $controller;
 		$action = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'index';
 		call_user_func(array($controller, $action));
