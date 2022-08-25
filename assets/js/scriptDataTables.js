bbd8a4542345a9cee6f262e66024a445013f827e
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-08-25 12:50:09 -0500

    Proyecto de Clase. Vistas: Tablas HTML y CSS

--- assets/js/scriptDataTables.js
+++ assets/js/scriptDataTables.js
@@ -2,24 +2,37 @@ $(document).ready(function() {
     var table = $('#table-data-tables').DataTable( {
         dom: 'Bfrtip',
         buttons: [
-            'copy',
-            'csv',
-            'excel',
-            'pdf',
-            {
-                extend: 'print',
-                text: 'Print all (not just selected)',
-                exportOptions: {
-                    modifier: {
-                        selected: null
-                    }
+        'copy',
+        'csv',
+        'excel',
+        'pdf',            
+        {
+            extend: 'print',
+            text: 'Imprimir Todo',
+            exportOptions: {
+                modifier: {
+                    selected: null
                 }
             }
+        }
         ],
-        select: true,
+        select: true,        
         rowReorder: {
             selector: 'td:nth-child(2)'
         },
-        responsive: true        
+        responsive: true,
+        language: {
+            search: 'Buscar',
+            zeroRecords: 'No hay registros para mostrar.',
+            emptyTable: 'La tabla está vacia.',
+            info: "Mostrando _START_ de _END_ de _TOTAL_ Registros.",
+            infoFiltered: "(Filtrados de _MAX_ Registros.)",
+            paginate: {
+                first: 'Primero',
+                previous: 'Anterior',
+                next: 'Siguiente',
+                last: 'Último'
+            }
+        }       
     } );
 } );
