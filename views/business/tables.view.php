ff9c377d008282cb38df18b5d4e12e0602e713db
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-08-25 09:34:53 -0500

    Proyecto de Clase. Vistas: Validación Front-End. sweetAlert()

--- views/business/tables.view.php
+++ views/business/tables.view.php
@@ -5,58 +5,59 @@
 			<p> &nbsp / &nbsp</p>
 			<a href="?c=Landing&a=tables">Tablas</a>
 		</div>
-		<section class="paginas tables">
-			<div class="table-scroll">
-				<table class="table">
-					<tr>
-						<th>Rol</th>
-						<th>Id</th>
-						<th>Nombres</th>
-						<th>Apellidos</th>
-						<th>Nº Documento</th>
-						<th>Correo</th>					
-						<th>Estado</th>
-						<th>Acciones</th>
-					</tr>
-					<tr>
-						<td>Admin</td>
-						<td>ADM-123</td>
-						<td>E. Albeiro</td>
-						<td>Ramos V.</td>
-						<td>1000987654</td>
-						<td>profealbeiro2020@gmail.com</td>					
-						<td>Activo</td>
-						<td>
-							<a href="#"><i class="fas fa-edit"></i></a>
-							<a href="#"><i class="fas fa-trash-alt"></i></a>
-						</td>
-					</tr>
-					<tr>
-						<td>Cliente</td>
-						<td>CLT-456</td>
-						<td>Rosa María</td>
-						<td>Godines Salazar</td>
-						<td>1001565465</td>
-						<td>rosa@cliente.com</td>					
-						<td>Activo</td>
-						<td>
-							<a href="#"><i class="fas fa-edit"></i></a>
-							<a href="#"><i class="fas fa-trash-alt"></i></a>
-						</td>
-					</tr>
-					<tr>
-						<td>Empleado</td>
-						<td>EMP-789</td>
-						<td>Victor Rogelio</td>
-						<td>Carranza Rincón</td>
-						<td>1005987896</td>
-						<td>victor@empleado.com</td>					
-						<td>Inactivo</td>
-						<td>
-							<a href="#"><i class="fas fa-edit"></i></a>
-							<a href="#"><i class="fas fa-trash-alt"></i></a>
-						</td>
-					</tr>
-				</table>
+		<section class="paginas">
+			<h3 class="subtitulo">Tablas en HTML y CSS</h3>
+			<div class="Tables table-scroll">				
+					<table class="table">
+						<tr>
+							<th>Rol</th>
+							<th>Id</th>
+							<th>Nombres</th>
+							<th>Apellidos</th>
+							<th>Nº Documento</th>
+							<th>Correo</th>					
+							<th>Estado</th>
+							<th>Acciones</th>
+						</tr>
+						<tr>
+							<td>Admin</td>
+							<td>ADM-123</td>
+							<td>E. Albeiro</td>
+							<td>Ramos V.</td>
+							<td>1000987654</td>
+							<td>profealbeiro2020@gmail.com</td>					
+							<td>Activo</td>
+							<td>
+								<a href="#"><i class="fas fa-edit"></i></a>
+								<a href="#"><i class="fas fa-trash-alt"></i></a>
+							</td>
+						</tr>
+						<tr>
+							<td>Cliente</td>
+							<td>CLT-456</td>
+							<td>Rosa María</td>
+							<td>Godines Salazar</td>
+							<td>1001565465</td>
+							<td>rosa@cliente.com</td>					
+							<td>Activo</td>
+							<td>
+								<a href="#"><i class="fas fa-edit"></i></a>
+								<a href="#"><i class="fas fa-trash-alt"></i></a>
+							</td>
+						</tr>
+						<tr>
+							<td>Empleado</td>
+							<td>EMP-789</td>
+							<td>Victor Rogelio</td>
+							<td>Carranza Rincón</td>
+							<td>1005987896</td>
+							<td>victor@empleado.com</td>					
+							<td>Inactivo</td>
+							<td>
+								<a href="#"><i class="fas fa-edit"></i></a>
+								<a href="#"><i class="fas fa-trash-alt"></i></a>
+							</td>
+						</tr>
+					</table>
 			</div>
 		</section>
