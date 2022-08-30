f26e569b4d68b6839e12dfa28e28722a7a7f7e5f
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-08-30 10:41:11 -0500

    Proyecto de Clase. Vistas: FullCalendar.js

--- views/roles/business/header.php
+++ views/roles/business/header.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/rowreorder/1.2.8/css/rowReorder.dataTables.min.css">
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.3.0/css/responsive.dataTables.min.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.css">
-		<link rel="stylesheet" href="assets/css/all.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
 	<link rel="stylesheet" href="assets/css/styles-model-flexbox.css">
 </head>
 <body>
