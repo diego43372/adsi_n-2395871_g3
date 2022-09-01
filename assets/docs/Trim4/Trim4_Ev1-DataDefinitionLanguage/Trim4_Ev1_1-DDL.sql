d5a35f442af2e1b1d475ecd2dd84c6e0f7fdf8b5
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-09-01 08:02:09 -0500

    Proyecto de Clase. Transformación MER a MR. III_Trim

--- assets/docs/Trim4/Trim4_Ev1-DataDefinitionLanguage/Trim4_Ev1_1-DDL.sql
+++ assets/docs/Trim4/Trim4_Ev1-DataDefinitionLanguage/Trim4_Ev1_1-DDL.sql
@@ -1,71 +1,146 @@
 -- -----------------------------------------------------
--- Estructura bbdd_pedidos
+-- ESTRUCTURA BBDD: bbdd_ventas
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS bbdd_pedidos 
-DEFAULT CHARACTER SET utf8;
-USE bbdd_pedidos;
+CREATE SCHEMA IF NOT EXISTS bbdd_ventas DEFAULT CHARACTER SET utf8;
+USE bbdd_ventas;
+
 -- -----------------------------------------------------
--- Tabla clientes
+-- TABLA: ROLES
 -- -----------------------------------------------------
-CREATE TABLE clientes (
-  codigo_cliente INT(11) NOT NULL AUTO_INCREMENT,
-  empresa VARCHAR(30),  
-  direccion VARCHAR(50),
-  poblacion VARCHAR(20),
-  telefono VARCHAR(15),
-  responsable VARCHAR(30),    
-  historial VARCHAR(30),
-  PRIMARY KEY (codigo_cliente)
-) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE ROLES (
+  codigo_rol INT NOT NULL AUTO_INCREMENT,
+  nombre_rol VARCHAR(50) NOT NULL,
+  PRIMARY KEY (codigo_rol)
+) ENGINE = InnoDB;
+
 -- -----------------------------------------------------
--- Tabla productos
+-- TABLA: USUARIOS
 -- -----------------------------------------------------
-CREATE TABLE productos (
-  codigo_articulo INT(11) NOT NULL AUTO_INCREMENT,
-  seccion VARCHAR(50),
-  nombre_articulo VARCHAR(100),
-  precio DECIMAL(10,2),
-  fecha DATE,  
-  importado TINYINT(1),
-  pais_origen VARCHAR(50),
-  foto VARCHAR(50),
-  PRIMARY KEY (codigo_articulo)
-) ENGINE = InnoDB DEFAULT CHARSET=utf8;
--- -----------------------------------------------------
--- Tabla pedidos
--- -----------------------------------------------------
-CREATE TABLE pedidos (
-  numero_pedido INT(11) NOT NULL AUTO_INCREMENT,
-  codigo_cliente INT(11) NOT NULL,
-  fecha_pedido DATE,
-  forma_pago VARCHAR(20),
-  descuento FLOAT(10,2),
-  enviado BIT(1),  
-  PRIMARY KEY (numero_pedido),
-  KEY fk_pedidos_clientes (codigo_cliente),
-  CONSTRAINT fk_pedidos_clientes
-    FOREIGN KEY (codigo_cliente)
-    REFERENCES clientes (codigo_cliente)
+CREATE TABLE USUARIOS (
+  codigo_rol INT NOT NULL,
+  codigo_user VARCHAR(10) NOT NULL,
+  identificacion_user INT NOT NULL,
+  nombres_user VARCHAR(50) NOT NULL,
+  apellidos_user VARCHAR(50) NOT NULL,
+  PRIMARY KEY (codigo_user),
+  UNIQUE INDEX uq_identificacion_user (identificacion_user ASC),
+  INDEX ind_usuarios_roles (codigo_rol ASC),
+  CONSTRAINT fk_usuarios_roles
+    FOREIGN KEY (codigo_rol)
+    REFERENCES ROLES (codigo_rol)
     ON DELETE CASCADE
     ON UPDATE CASCADE
-) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB;
+
 -- -----------------------------------------------------
--- Tabla productos_pedidos
+-- TABLA: CREDENCIALES
 -- -----------------------------------------------------
-CREATE TABLE productos_pedidos (
-  numero_pedido INT(11) NOT NULL,
-  codigo_articulo INT(11) NOT NULL,
-  unidades INT(11) NOT NULL,
-  KEY fk_productos_pedidos_productos (codigo_articulo),
-  KEY fk_productos_pedidos_pedidos (numero_pedido),
-  CONSTRAINT fk_productos_pedidos_pedidos
-    FOREIGN KEY (numero_pedido)
-    REFERENCES pedidos (numero_pedido)
+CREATE TABLE CREDENCIALES (
+  codigo_user VARCHAR(10) NOT NULL,
+  correo_user VARCHAR(100) NOT NULL,
+  pass_user VARCHAR(150) NOT NULL,
+  UNIQUE INDEX uq_correo_user (correo_user ASC),
+  INDEX ind_credenciales_usuarios (codigo_user ASC),
+  CONSTRAINT fk_credenciales_usuarios
+    FOREIGN KEY (codigo_user)
+    REFERENCES USUARIOS (codigo_user)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- TABLA: MENSAJES
+-- -----------------------------------------------------
+CREATE TABLE MENSAJES (
+  codigo_user VARCHAR(10) NOT NULL,
+  fecha_mensaje DATE NOT NULL,
+  asunto_mensaje VARCHAR(50) NOT NULL,
+  descripcion_mensaje VARCHAR(300) NOT NULL,
+  INDEX ind_mensajes_usuarios (codigo_user ASC),
+  CONSTRAINT fk_mensajes_usuarios
+    FOREIGN KEY (codigo_user)
+    REFERENCES USUARIOS (codigo_user)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- TABLA: CATEGORÍAS
+-- -----------------------------------------------------
+CREATE TABLE CATEGORIAS (
+  codigo_categoria INT NOT NULL AUTO_INCREMENT,
+  nombre_categoria VARCHAR(50) NOT NULL,
+  PRIMARY KEY (codigo_categoria)
+) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- TABLA: PRODUCTOS
+-- -----------------------------------------------------
+CREATE TABLE PRODUCTOS (
+  codigo_categoria INT NOT NULL,
+  codigo_producto VARCHAR(10) NOT NULL,
+  nombre_producto VARCHAR(50) NOT NULL,
+  precio_producto DECIMAL(10,2) NOT NULL,
+  unidad_producto DECIMAL(5,2) NOT NULL,
+  medida_producto VARCHAR(20) NOT NULL,
+  PRIMARY KEY (codigo_producto),
+  INDEX ind_productos_categorias (codigo_categoria ASC),
+  CONSTRAINT fk_productos_categorias
+    FOREIGN KEY (codigo_categoria)
+    REFERENCES CATEGORIAS (codigo_categoria)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- TABLA: PEDIDOS
+-- -----------------------------------------------------
+CREATE TABLE PEDIDOS (
+  codigo_pedido VARCHAR(10) NOT NULL,
+  fecha_pedido DATE NOT NULL,
+  total_pl_pedido DECIMAL(10,2) NOT NULL,
+  iva_pedido DECIMAL(10,2) NOT NULL,
+  total_pr_pedido DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (codigo_pedido))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- TABLA: LISTA_PRODUCTOS
+-- -----------------------------------------------------
+CREATE TABLE LISTA_PRODUCTOS (
+  codigo_pedido VARCHAR(10) NOT NULL,
+  codigo_producto VARCHAR(10) NOT NULL,
+  cantidad_productos INT NOT NULL,
+  INDEX ind_lista_productos_pedidos (codigo_pedido ASC),
+  INDEX ind_lista_productos_productos (codigo_producto ASC),
+  CONSTRAINT fk_lista_productos_pedidos
+    FOREIGN KEY (codigo_pedido)
+    REFERENCES PEDIDOS (codigo_pedido)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT fk_productos_pedidos_productos
-    FOREIGN KEY (codigo_articulo)
-    REFERENCES productos (codigo_articulo)
+  CONSTRAINT fk_lista_productos_productos
+    FOREIGN KEY (codigo_producto)
+    REFERENCES PRODUCTOS (codigo_producto)
     ON DELETE CASCADE
     ON UPDATE CASCADE
-) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- TABLA: CREDENCIALES_PEDIDOS
+-- -----------------------------------------------------
+CREATE TABLE CREDENCIALES_PEDIDOS (
+  codigo_user VARCHAR(10) NOT NULL,
+  codigo_pedido VARCHAR(10) NOT NULL,
+  INDEX ind_credenciales_pedidos_credenciales (codigo_user ASC),
+  INDEX ind_credenciales_pedidos_pedidos (codigo_pedido ASC),
+  CONSTRAINT fk_credenciales_pedidos_credenciales
+    FOREIGN KEY (codigo_user)
+    REFERENCES CREDENCIALES (codigo_user)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT fk_credenciales_pedidos_pedidos
+    FOREIGN KEY (codigo_pedido)
+    REFERENCES PEDIDOS (codigo_pedido)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+) ENGINE = InnoDB;
